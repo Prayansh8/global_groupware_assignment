e3b12103561999046ac6fdb6283963920e31574a
Prayansh <91784825+Prayansh8@users.noreply.github.com>
2025-03-30 02:14:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
           )}
         </Toolbar>
       </AppBar>
-      <Container maxWidth="lg" sx={{ marginTop: '10vh' }}>
+      <Container maxWidth="lg" sx={{ marginTop: '4vh', marginX: '1vh' }}>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={token ? <UserList /> : <Navigate to="/login" replace />} />
